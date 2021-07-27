ecb9fbfeee7036fd43314edcf9a5b45580a52f8a
AlekseyDildikov <adildikov@gmail.com>
2021-07-27 16:34:22 +0300

    add Display component & edit some styles

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,10 +1,13 @@
 import React from "react";
+import Display from "../Display/Display";
 import Form from "../Form/Form";
+import "./style.css";
 
 export default function App() {
   return (
-    <div>
+    <div className="app_wrapper">
       <Form />
+      <Display />
     </div>
   );
 }
---
+++ src/components/Display/Display.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import "./style.css";
+
+export default function Display() {
+  return <div className="display_wrapper"></div>;
+}
