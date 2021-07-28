21ac1ac072ded38934a0be3ae203aa7f532c02f1
AlekseyDildikov <adildikov@gmail.com>
2021-07-28 14:39:24 +0300

    add localStorage

--- src/redux/reducers/data.js
+++ src/redux/reducers/data.js
@@ -1,16 +1,26 @@
 import { PUT_DATA } from "../constants";
+import { load } from "redux-localstorage-simple";
 
-const initialState = {
-  lastName: "",
-  firstName: "",
-  middleName: "",
-  study_work: "",
-  dateOfBirth: "",
-  email: "",
-  other: "",
-};
-
-const data = (state = initialState, { type, value, field }) => {
+let initialState = load({ namespace: "data" });
+if (
+  initialState === undefined ||
+  initialState === null ||
+  initialState === {} ||
+  !initialState.data
+) {
+  initialState = {
+    data: {
+      lastName: "",
+      firstName: "",
+      middleName: "",
+      study_work: "",
+      dateOfBirth: "",
+      email: "",
+      other: "",
+    },
+  };
+}
+const data = (state = initialState.data, { type, value, field }) => {
   switch (type) {
     case PUT_DATA:
       return {
